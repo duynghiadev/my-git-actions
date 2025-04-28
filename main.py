8e50488a66737db4153815ac2a1a9361a661c332
duynghiadev <duynghia22302@gmail.com>
2025-04-28 16:59:07 +0700

    refactor: improve code style and fix linting issues

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-def hello():
-    print("hi")
+def badFunction( x,y ):
+    z=x+y
+    if(z>10):
+      print('result is:'+str(z))
+    return      z
 
+# unused import
+import os
+import sys
 
-def bye():
-    print("bye")
+# undefined variable
+print(undefined_variable)
 
-
-print(hello())
+# bad whitespace and missing docstring
+class badClass:
+  def __init__(self):
+    self.x=1
+    self.y=2
